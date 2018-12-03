ba088f976513f3272b62d1d3dc7180feb0faaea0
Zhichang Yu <yuzhichang@gmail.com>
2018-12-03 15:26:05 +0800

    replaced dep with mod

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 GIT_SHA=$(git rev-parse --short HEAD || echo "GitNotFound")
 BUILD_TIME=$(date --iso-8601=seconds)
-CGO_ENABLED=0 go build -ldflags "-X main.GitSHA=${GIT_SHA} -X main.BuildTime=${BUILD_TIME}"
+GO111MODULE=on CGO_ENABLED=0 go build -ldflags "-X main.GitSHA=${GIT_SHA} -X main.BuildTime=${BUILD_TIME}"
